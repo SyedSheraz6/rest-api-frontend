eb2e893c2eafb4f55e7e5c48b84eec382d56e669
Syed Sheraz <sherazsyed16@gmail.com>
2024-10-02 19:39:23 +0500

    Added authentication, bearer token

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -51,7 +51,11 @@ class Feed extends Component {
       page--;
       this.setState({ postPage: page });
     }
-    fetch('http://localhost:8080/feed/posts?page=' + page)
+    fetch('http://localhost:8080/feed/posts?page=' + page, {
+      headers: {
+        Authorization: 'Bearer ' + this.props.token
+      }
+    })
       .then(res => {
         if (res.status !== 200) {
           throw new Error('Failed to fetch posts.');
@@ -126,7 +130,10 @@ class Feed extends Component {
 
     fetch(url, {
       method: method,
-      body: formData
+      body: formData,
+      headers: {
+        Authorization: 'Bearer ' + this.props.token
+      }
     })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
@@ -135,7 +142,6 @@ class Feed extends Component {
         return res.json();
       })
       .then(resData => {
-        console.log('resData', resData)
         const post = {
           _id: resData.post._id,
           title: resData.post.title,
@@ -179,7 +185,10 @@ class Feed extends Component {
   deletePostHandler = postId => {
     this.setState({ postsLoading: true });
     fetch('http://localhost:8080/feed/post/' + postId, {
-      method: 'DELETE'
+      method: 'DELETE',
+      headers: {
+        Authorization: 'Bearer ' + this.props.token
+      }
     })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
